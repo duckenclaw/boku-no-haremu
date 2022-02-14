21ca6da77a663fc98e41160d6650d1512ac7ff3d
MaMorde <mamorde@yandex.ru>
2022-02-14 16:12:15 +0300

    fix

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import classNames from 'classnames'
 import s from '/styles/Home.module.css'
 import { Footer } from './shared-ui/footer/footer'
-import { TokenisedEconomy } from './TokenisedEconomy'
 
 type Props = {
   className?: string
@@ -50,7 +49,6 @@ const MainContent: React.FC<Props> = ({ className }) => {
           </a>
         </div>
       </main>
-      <TokenisedEconomy />
       <Footer />
     </div>
   )
