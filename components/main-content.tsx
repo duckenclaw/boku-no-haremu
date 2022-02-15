c556c2ec6a34b891c17221e86ff1cf681a4fa57a
MaMorde <mamorde@yandex.ru>
2022-02-15 18:05:45 +0300

    fix

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import classNames from 'classnames'
 import s from '/styles/Home.module.css'
-import { Footer } from './shared-ui/footer/footer'
 import RoadMap from './road_map/road_map'
 import { GrowWaifu } from './GrowWaifu'
+import { TokenisedEconomy } from './TokenisedEconomy'
+import { ExclusiveNfts } from './ExclusiveNfts'
 
 type Props = {
   className?: string
@@ -13,10 +14,11 @@ const MainContent: React.FC<Props> = ({ className }) => {
   return (
     <div className={classNames(className, s.container)}>
       <main className={s.main}>
+        <TokenisedEconomy />
+        <ExclusiveNfts />
         <GrowWaifu />
         <RoadMap />
       </main>
-      <Footer />
     </div>
   )
 }
