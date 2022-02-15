57990c5798310dfff21dea33953c30726b3cb4f5
“Victor <justicejesus1237@gmail.com>
2022-02-15 13:59:25 +0300

    road map is done

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import classNames from 'classnames'
 import s from '/styles/Home.module.css'
 import { Footer } from './shared-ui/footer/footer'
+import RoadMap from './road_map/road_map'
 
 type Props = {
   className?: string
@@ -11,9 +12,10 @@ const MainContent: React.FC<Props> = ({ className }) => {
   return (
     <div className={classNames(className, s.container)}>
       <main className={s.main}>
-        <div className={s.fontDisplay}>aaaaaaaaa</div>
+        {/* <div className={s.fontDisplay}>aaaaaaaaa</div>
         <div className={s.fontDisplayBold}>aaaaaaaaa</div>
-        <div className={s.fontBadaBoom}>aaaaaaaaa</div>
+        <div className={s.fontBadaBoom}>aaaaaaaaa</div> */}
+        <RoadMap />
       </main>
       <Footer />
     </div>
