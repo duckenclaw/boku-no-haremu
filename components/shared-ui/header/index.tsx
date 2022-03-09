a060df4aaaf409415d357b86702db0dcee6d7e71
Darja <alehina321@gmail.com>
2022-03-09 18:56:24 +0300

    header mobile

--- components/shared-ui/header/index.tsx
+++ components/shared-ui/header/index.tsx
@@ -4,6 +4,7 @@ import s from './styles.module.scss'
 import { Navigation } from './navigation'
 import { Socials } from '../socials'
 import Container from '../container'
+import { MobileMenu } from './mobile-menu/mobile-menu'
 
 type Props = {
   className?: string
@@ -14,6 +15,7 @@ const Header: React.FC<Props> = ({ className }) => {
     <header className={classNames(className, s.container)}>
       <Container className={s.wrapper}>
         <Navigation />
+        <MobileMenu />
         <Socials />
       </Container>
     </header>
