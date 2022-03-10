501bf3e24067fee08e79211a01d85124de6496bf
MaMorde <mamorde@yandex.ru>
2022-03-10 18:43:53 +0300

    fix menu

--- components/shared-ui/header/navigation/index.tsx
+++ components/shared-ui/header/navigation/index.tsx
@@ -6,48 +6,46 @@ type Props = {
   className?: string
 }
 
-const Navigation: React.FC<Props> = ({ className }) => {
-  return (
-    <nav className={classNames(className, s.container)}>
-      <ul className={s.navList}>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            TOKENS
-          </a>
-        </li>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            NFTS
-          </a>
-        </li>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            GROWTH
-          </a>
-        </li>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            BLENDING
-          </a>
-        </li>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            TYPES
-          </a>
-        </li>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            ROAD MAP
-          </a>
-        </li>
-        <li className={s.navItem}>
-          <a className={s.navLink} href="#">
-            WHITE PAPER
-          </a>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+const Navigation: React.FC<Props> = ({ className }) => (
+  <nav className={classNames(className, s.container)}>
+    <ul className={s.navList}>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          TOKENS
+        </a>
+      </li>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          NFTS
+        </a>
+      </li>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          GROWTH
+        </a>
+      </li>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          BLENDING
+        </a>
+      </li>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          TYPES
+        </a>
+      </li>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          ROAD MAP
+        </a>
+      </li>
+      <li className={s.navItem}>
+        <a className={s.navLink} href="#">
+          WHITE PAPER
+        </a>
+      </li>
+    </ul>
+  </nav>
+)
 
 export { Navigation }
