0a843b8563025edb89237f77d1ead869a569e7fe
Darja <alehina321@gmail.com>
2022-03-18 18:49:42 +0300

    header

--- game/game_header/index.tsx
+++ game/game_header/index.tsx
@@ -1,16 +1,45 @@
 import { useWax } from 'contexts/wax_context'
+import s from './game_header.module.scss'
+
+type Res = {
+  value: string
+  logo: string
+}
+
+const res: Res[] = [
+  {
+    value: '0',
+    logo: 'nyan',
+  },
+  {
+    value: '0',
+    logo: 'crystal',
+  },
+  {
+    value: '0',
+    logo: 'simptetix',
+  },
+  {
+    value: '0',
+    logo: 'bento',
+  },
+]
 
 export const GameHeader = () => {
   const { wax } = useWax()
   return (
-    <header>
-      <div>{wax?.userAccount}</div>
-      <div>
-        <div>res 0</div>
-        <div>res 0</div>
-        <div>res 0</div>
-        <div>res 0</div>
-        <div>I</div>
+    <header className={s.container}>
+      <div className={s.profile_info}>{wax?.userAccount}</div>
+      <div className={s.cards}>
+        {res?.map((item: Res) => (
+          <div key={item.logo} className={s.res}>
+            <img className={s.cardPriceIcon} src={`images/${item.logo}.png`} />
+            {item.value}
+          </div>
+        ))}
+        <div>
+          <img src="images/Icon.png" />
+        </div>
       </div>
     </header>
   )
