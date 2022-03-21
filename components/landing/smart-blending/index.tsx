d80b3574d8917801b340580afbff8e0bd131c195
Darja <alehina321@gmail.com>
2022-03-21 20:04:01 +0300

    fix-fusion

--- components/landing/smart-blending/index.tsx
+++ components/landing/smart-blending/index.tsx
@@ -15,12 +15,7 @@ export const SmartBlending: React.FC<SmartBlendingProps> = ({ className }) => (
   <section className={classNames(className, s.section)}>
     <div className={s.top}></div>
     <Container className={s.container}>
-      <TextHeader
-        title="SMART FUSION"
-        subtitle="You can fuse waifus together in order to get another one that you will like even more."
-        className={s.textHeader}
-        id="blending"
-      />
+      <TextHeader title="SMART FUSION" className={s.textHeader} id="blending" />
       <div className={s.content}>
         <div className={s.column}>
           <img className={s.imageTop} alt="" src="/images/mimi_3.png" />
