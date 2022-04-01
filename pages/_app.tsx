54fe33ff9481c92cd06ec9f9896f1ee303c3321c
Evgeny Taktarov <je.mouth@gmail.com>
2022-04-01 19:44:06 +0700

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,16 @@ import Modal from 'react-modal'
 Modal.setAppElement('#__next')
 
 function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
