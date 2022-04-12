fb82c849e0d25a9da73bbf01b1d36ca100804517
MaMorde <mamorde@yandex.ru>
2022-04-12 18:32:46 +0300

    fix footer 2.0

--- components/landing/footer/index.tsx
+++ components/landing/footer/index.tsx
@@ -5,35 +5,37 @@ import { Socials } from 'components/shared-ui/socials'
 
 export const Footer = () => (
   <div className={s.container}>
-    <div className={s.left}>
-      <Socials className={s.socials} />
-      <p className={s.privateDesktop}>
-        © 2022 BOKU NO COMPANY LIMITED. Private Limited Company
-      </p>
-    </div>
-    <div className={s.contact_text}>
-      <div className={s.heading}>
-        Company address: <br className={s.br} />
-        <br className={s.br} />
-        Unit 1603, 16th Floor, The L. Plaza, 367 - 375 Queen's Road Central,
-        Sheung Wan, Hong Kong, China
+    <div className={s.inner}>
+      <div className={s.left}>
+        <Socials className={s.socials} />
+        <p className={s.privateDesktop}>
+          © 2022 BOKU NO COMPANY LIMITED. Private Limited Company
+        </p>
       </div>
-      <div className={s.heading}>
-        Email: <br className={s.br} />
-        <br className={s.br} />
-        <a href="mailto: 'info@bokunoharemu.com'">info@bokunoharemu.com</a>
-      </div>
-      <div className={s.heading}>
-        CEO: <br className={s.br} />
-        <br className={s.br} /> Elza Gafurova
-      </div>
-      <div className={s.heading}>
-        Incorporation number: <br className={s.br} />
-        <br className={s.br} />
-        3140973
-      </div>
-      <div className={s.privateMobile}>
-        <div>© 2022 BOKU NO COMPANY LIMITED. Private Limited Company</div>
+      <div className={s.contact_text}>
+        <div className={s.heading}>
+          Company address: <br className={s.br} />
+          <br className={s.br} />
+          Unit 1603, 16th Floor, The L. Plaza, 367 - 375 Queen's Road Central,
+          Sheung Wan, Hong Kong, China
+        </div>
+        <div className={s.heading}>
+          Email: <br className={s.br} />
+          <br className={s.br} />
+          <a href="mailto: 'info@bokunoharemu.com'">info@bokunoharemu.com</a>
+        </div>
+        <div className={s.heading}>
+          CEO: <br className={s.br} />
+          <br className={s.br} /> Elza Gafurova
+        </div>
+        <div className={s.heading}>
+          Incorporation number: <br className={s.br} />
+          <br className={s.br} />
+          3140973
+        </div>
+        <div className={s.privateMobile}>
+          <div>© 2022 BOKU NO COMPANY LIMITED. Private Limited Company</div>
+        </div>
       </div>
     </div>
   </div>
