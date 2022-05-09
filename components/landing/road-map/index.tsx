0d2e804a06345ef2adeb5ab7284523390a4fe4b0
Denis Pelevin <pelevin.d.y@gmail.com>
2022-05-09 18:54:40 +0700

    feat: roadmap add text styles

--- components/landing/road-map/index.tsx
+++ components/landing/road-map/index.tsx
@@ -11,21 +11,21 @@ export const RoadMap = () => {
       number: 'March 2022',
       title: 'Pre-Production',
       description:
-        'We assembled the team, finished ideation of our project and started working on the game. We launched a Discord server, Twitter page and Medium of Boku no Haremu. It is only a start on our journey to give you the Waifus you deserve.',
+        'We assembled the team, finished ideation of our project and started working on the game. We launched a <b>Discord server, Twitter</b> page and Medium of Boku no Haremu. It is only a start on our journey to give you the Waifus you deserve.',
       image: 'images/road_map/image1.png',
     },
     {
       number: 'April 2022',
       title: 'Verification on AtomicHub',
       description:
-        'We have done all the neccesary pre-production work to get our project verified on AtomicHub. Discord server is up, Smartcontract and Webpage are created, Twitter page is up and we’re all set to go! Now you can invest in our project knowing full well that Boku no Haremu is actually happening!',
+        'We have done all the neccesary pre-production work to get our project verified on AtomicHub. <b>Discord server</b> is up, <b>Smartcontract</b> and <b>Webpage</b> are created, Twitter page is up and we’re all set to go! Now you can invest in our project knowing full well that <b>Boku no Haremu</b> is actually happening!',
       image: 'images/road_map/image2.webp',
     },
     {
       number: 'May 2022',
       title: 'May 10 - Mintpass sale day 1 ',
       description:
-        'On the release day of the mintpasses you can buy them for 179 WAX and there will be 200 of them. This will be the lowest price in Boku no Haremu’s history so don’t miss out on your chance! Remember, that you can participate in the sale ONLY if you have WHITELIST NFT  in wallet.',
+        'On the release day of the mintpasses you can buy them for <span style="color: #6CCAF3">179 WAX</span> and there will be 200 of them. This will be the <b>lowest price</b> in <b>Boku no Haremu’s</b> history so don’t miss out on your chance! Remember, that you can participate in the sale <b>ONLY</b> if you have <b>WHITELIST NFT</b> in wallet.',
       image: 'images/road_map/image3.webp',
     },
     {
@@ -41,14 +41,14 @@ export const RoadMap = () => {
       number: '',
       title: 'May 13 - Mintpass sale day 2',
       description:
-        'On the second day of the sale the price will go up to 359 WAX  and there will be only 300 mintpasses for sale. The price will only go up from this point.',
+        'On the second day of the sale the price will go up to <span style="color: #6CCAF3">359 WAX</span> and there will be only 300 mintpasses for sale. The price will only go up from this point.',
       image: 'images/road_map/image4.webp',
     },
     {
       number: '',
       title: 'May 15 - Mintpass sale day 3',
       description:
-        'It’s the final day of the mintpass sale, and the price is now 589 WAX, there is only 300 of them! Get your mintpasses, this will be your last chance to get into the most progressive project on WAX - Boku no Haremu.',
+        'It’s the final day of the mintpass sale, and the price is now <span style="color: #6CCAF3">589 WAX</span>, there is only 300 of them! Get your mintpasses, this will be your <b>last chance</b> to get into the most <b>progressive project</b> on <b>WAX - Boku no Haremu</b>.',
       image: 'images/road_map/image6.webp',
     },
     {
@@ -77,7 +77,7 @@ export const RoadMap = () => {
       number: '',
       title: 'NFT Packs sale',
       description:
-        'The beginning of a legendary tale of Harem Masters. What Waifu will you get? The launch of the game is coming up so you better be ready!',
+        'The beginning of a legendary tale of <b>Harem Masters</b>. What Waifu will you get? The launch of the game is coming up so you better be ready!',
       image: 'images/road_map/image13.webp',
     },
     {
@@ -91,7 +91,7 @@ export const RoadMap = () => {
       number: '',
       title: 'Resource Packs sale',
       description:
-        'Gifts from your waifus gathered in one Resources are needed to start the mining the resources, you can check our Whitepaper to know the mechanics, so be ready for some more packs that will ensure prosperity of your Haremu.',
+        'Gifts from your waifus gathered in one Resources are needed to start the mining the resources, you can check our <p>Whitepaper</p> to know the mechanics, so be ready for some more packs that will ensure prosperity of your Haremu.',
       image: 'images/road_map/image8.webp',
     },
     {
@@ -105,14 +105,14 @@ export const RoadMap = () => {
       number: 'July 2022',
       title: 'Generative NFTs of the 5th level',
       description:
-        'This is the final step, the last obstacle you have to overcome, the end of the journey to your goal — a fully generated WAIFU. This is a race now and you know you have to win no matter the cost, there will be only 1000 level 5 Waifus of every type so you better hurry!',
+        'This is the final step, the last obstacle you have to overcome, the end of the journey to your goal — a <b>fully generated WAIFU</b>. This is a race now and you know you have to win no matter the cost, there will be only <b>1000 level 5 Waifus of every type</b> so you better hurry!',
       image: 'images/road_map/image11.webp',
     },
     {
       number: '',
       title: 'Bridge to EVM blockchains',
       description:
-        'We’re all set for the migration of your Harem, now you can have your Waifu on OpenSea so everyone can see what path you have taken, what happened in your journey and what obstacles you have overcome to get your Waifu!',
+        'We’re all set for the migration of your Harem, now you can have your <b>Waifu</b> on <b>OpenSea</b> so everyone can see what path you have taken, what happened in your journey and what obstacles you have overcome to get your Waifu!',
       image: 'images/road_map/image12.webp',
     },
   ]
@@ -163,9 +163,8 @@ export const RoadMap = () => {
                         ? s.description
                         : s.description_secret
                     }
-                  >
-                    {item.description}
-                  </div>
+                    dangerouslySetInnerHTML={{ __html: item.description }}
+                  />
                 </div>
                 {item.image && (
                   <div className={s.image_block}>
