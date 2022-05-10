bfc7bda07c096530a4b67f40aec9735e02835b89
“Victor <justicejesus1237@gmail.com>
2022-05-10 15:34:16 +0300

    fix build

--- components/landing/hero/sakura_animation.tsx
+++ components/landing/hero/sakura_animation.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useLayoutEffect, useRef } from 'react'
-import { useMediaQuery } from 'react-responsive'
+// import { useMediaQuery } from 'react-responsive'
 import { runSakuraAnimation, unmountSakuraAnimation } from '../canvas/canvas'
 
 type SakuraAnimationProps = {
@@ -8,15 +8,22 @@ type SakuraAnimationProps = {
 
 export const SakuraAnimation = ({ className }: SakuraAnimationProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const isMobile = useMediaQuery({
-    query: '(min-width: 768px)',
-  })
+  // const isMobile = useMediaQuery({
+  //   query: '(min-width: 768px)',
+  // })
+  // useLayoutEffect(() => {
+  //   runSakuraAnimation(canvasRef.current)
+  //   return () => {
+  //     unmountSakuraAnimation()
+  //   }
+  // }, [isMobile])
   useLayoutEffect(() => {
-    runSakuraAnimation(canvasRef.current)
-    return () => {
-      unmountSakuraAnimation()
+    if (window.width > 768) {
+      runSakuraAnimation(canvasRef.current)
+      return () => {
+        unmountSakuraAnimation()
+      }
     }
-  }, [isMobile])
-
+  }, [])
   return <canvas className={className} ref={canvasRef} id="sakura" />
 }
