88164762e67353d4ec748c3a77a156cae676b544
Denis Pelevin <pelevin.d.y@gmail.com>
2022-05-30 17:51:18 +0700

    fix: fuse different cards

--- game/screens/fusion/index.tsx
+++ game/screens/fusion/index.tsx
@@ -41,10 +41,11 @@ export const Fusion = () => {
   const disableFuse = useMemo(
     () =>
       isFuseLoading ||
-      (cards.some((c) => !c) &&
-        cards
-          .filter((c) => c?.template_id)
-          .every((c, _, array) => c?.template_id === array[0]?.template_id)),
+      cards.every((card) => !card) ||
+      cards.every(
+        (card) =>
+          !!cards.find((item) => item?.template_id !== card?.template_id)
+      ),
     [cards, isFuseLoading]
   )
 
